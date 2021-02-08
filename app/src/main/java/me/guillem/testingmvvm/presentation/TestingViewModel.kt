6170b109dd66164f52b7d72da616b0b4db8092ac
guillem <guillempejo@gmail.com>
2021-02-08 23:02:02 +0100

    first commit

---
+++ app/src/main/java/me/guillem/testingmvvm/presentation/TestingViewModel.kt
@@ -0,0 +1,16 @@
+package me.guillem.testingmvvm.presentation
+
+import android.view.KeyEvent
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+
+/**
+ ** Created by Guillem on 08/02/21.
+ */
+class TestingViewModel : ViewModel() {
+
+    fun testing()= LiveData(Dispatchers.IO)
+
+
+}
